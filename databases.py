097db6084e3fd62692919341786996448720f5d2
exp-of-betrayal <glebisi@yandex.ru>
2023-02-27 07:02:40 +0400

    созданы таблички

--- databases.py
+++ databases.py
@@ -1,24 +1,16 @@
 import sqlite3
-# https://metanit.com/sql/mysql/2.5.php
-try:
-    sqlite_connection = sqlite3.connect('sqlite_python.db')
-    sqlite_create_table_query = '''CREATE TABLE auth (
-                                Id INT PRIMARY KEY AUTOINCREMENT,
-                                name TEXT NOT NULL,
-                                email text NOT NULL UNIQUE,
-                                password VARCHAR(16) NOT NULL);'''
 
+
+def regestry_new_acc(username: str, password: str, email: str, avatar_image=None):
+    sqlite_connection = sqlite3.connect('general_database.db')
+    sqlite_create_table_query = ''' select max(Id) from Auth '''
     cursor = sqlite_connection.cursor()
-    print("База данных подключена к SQLite")
-    cursor.execute(sqlite_create_table_query)
+    user_id = cursor.execute(sqlite_create_table_query).fetchone()
     sqlite_connection.commit()
-    print("Таблица SQLite создана")
+    user_id = user_id[0]
 
     cursor.close()
+    sqlite_connection.close()
 
-except sqlite3.Error as error:
-    print("Ошибка при подключении к sqlite", error)
-finally:
-    if (sqlite_connection):
-        sqlite_connection.close()
-        print("Соединение с SQLite закрыто")
+
+regestry_new_acc("test", "0000", "netu@mail.ru")
