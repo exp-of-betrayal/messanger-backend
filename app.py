8156ae57cf243bea88f0f9bb80271947af4393c5
exp-of-betrayal <glebisi@yandex.ru>
2023-03-01 20:22:52 +0400

    Доделаны регистрация и авторизация
    авторизация и регистрация полностью доделаны, несчитая отсутствующих восстановления пароля, разлогинивания и удаления токенов

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
 from configs.registration_patterns import email_pattern, nickname_pattern, password_pattern
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, redirect
 import re
+from databases import data_correctly, registry_new_acc, check_token, generate_token, get_id_by_email
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
 
 @app.route('/')
 def index():
+    # attention! code when will be ниже mast be deleted
+    resp = make_response("you are in main page <a href='/login'>login</a>, <a href='/registration'>reg</a>")
+    resp.set_cookie('token', '', 60 * 60 * 24)
+    resp.set_cookie('id', '', 60 * 60 * 24)
+    # it use for clearing cockie
     return "you are in main page <a href='/login'>login</a>, <a href='/registration'>reg</a>"
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.cookies.get('token'):
+        id = int(request.cookies.get('id'))
+        token = request.cookies.get('token')
+        if check_token(int(id), token) == 0:
+            return redirect('/me')
     if request.method == "POST":
-        return "post to login"
+        email = request.form.get("email")
+        password = request.form.get("password")
+        if data_correctly(email, password) == 0:
+            response = make_response(redirect("/me"))
+            id = get_id_by_email(email=email)
+            token = generate_token()
+            print(f"id->{id},token->{token}.")
+            response.set_cookie('token', token, 60 * 60 * 24)
+            response.set_cookie('id', str(id), 60 * 60 * 24)
+            return response
+        return render_template("account.html", error_log="password or email incorrect")
     return render_template("account.html")
 
 
@@ -37,18 +58,47 @@ def verify_reg(name, email, password):
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
+    if request.cookies.get('token'):
+        id = int(request.cookies.get('id'))
+        token = request.cookies.get('token')
+        if check_token(int(id), token) == 0:
+            return redirect('/me')
     if request.method == "POST":
-        print(request.form.values())
         name = request.form.get("nickname")
         email = request.form.get("email")
         password = request.form.get("password")
+
         errors = verify_reg(name, email, password)
         if not errors:
-            return "succesfull registration"
+            token, user_id = registry_new_acc(username=name, password=password, email=email)
+            res = make_response(redirect("me"))
+            res.set_cookie('token', str(token), max_age=60 * 60 * 24)
+            res.set_cookie('id', str(user_id), max_age=60 * 60 * 24)
+            return res
         else:
             return render_template("registration.html", errors=errors, values=[name, email, password])
 
     return render_template("registration.html")
+
+
+@app.route('/me', methods=['GET', 'POST'])
+def user_page():
+    if not request.cookies.get('token'):
+        return redirect("/login")
+    else:
+        token = request.cookies.get('token')
+        id = request.cookies.get('id')
+        if check_token(int(id), token):
+            resp = make_response(redirect("/login"))
+            resp.set_cookie('token', '', 60 * 60 * 24)
+            resp.set_cookie('id', '', 60 * 60 * 24)
+        else:
+            return "Ну типо вы авторизованы (типо)"
+
+
+@app.route('/forgot-password')
+def forgot():
+    return redirect('https://yandex.ru/images/search?from=tabbar&text=%D0%B3%D0%BB%D0%B8%D1%86%D0%B8%D0%BD%20%D1%80%D0%B5%D0%BA%D0%BB%D0%B0%D0%BC%D0%B0')
 
 
 app.run()
--- databases.py
+++ databases.py
@@ -1,16 +1,89 @@
+import random
 import sqlite3
+import string
+import time
 
 
-def regestry_new_acc(username: str, password: str, email: str, avatar_image=None):
-    sqlite_connection = sqlite3.connect('general_database.db')
-    sqlite_create_table_query = ''' select max(Id) from Auth '''
-    cursor = sqlite_connection.cursor()
-    user_id = cursor.execute(sqlite_create_table_query).fetchone()
-    sqlite_connection.commit()
+def generate_token():
+    part1 = str(hash(time.time()))
+    part1 = part1.replace("1", "a").replace("2", "b").replace("3", "c").replace("4", "d").replace("5", "e")
+    part1 = part1.replace("6", "f").replace("7", "g").replace("8", "h").replace("9", "i")
+    part2 = "".join(random.choices(string.digits + string.ascii_lowercase, k=15))
+    token = "s.ch." + part1 + "." + part2
+    return token
+
+
+def check_token(id: int, token: str):
+    connection = sqlite3.connect("general_database.db")
+    cursor = connection.cursor()
+    data = cursor.execute(
+        "SELECT * FROM Tokens WHERE Token = (?) and UserId = (?);", (token, id))
+    if data == "None":
+        return 1
+    else:
+        return 0
+
+
+def write_token(id: int, token: str):
+    connection = sqlite3.connect("general_database.db")
+    cursor = connection.cursor()
+    cursor.execute('''INSERT into Tokens (UserId, Token) VALUES (?, ?)''',
+                   (id, token)).fetchone()
+
+
+def registry_new_acc(username: str, password: str, email: str, phone=None, avatar_image=None):
+    connection = sqlite3.connect('general_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('''INSERT into Auth (Email, Password) VALUES (?, ?)''', (email, password)).fetchone()
+    user_id = cursor.execute('''select max(Id) from Auth''').fetchone()
     user_id = user_id[0]
+    token = generate_token()
+    connection.commit()
+    cursor.execute("""INSERT into General_info (UserId, Username, Email, Password) VALUES (?, ?, ?, ?)""",
+                   (user_id, username, email, password)).fetchone()
+    connection.commit()
+    cursor.execute("INSERT into Tokens (UserId, Token) values (?, ?)", (user_id, token))
+    connection.commit()
 
     cursor.close()
-    sqlite_connection.close()
+    connection.close()
+    return token, user_id
 
 
-regestry_new_acc("test", "0000", "netu@mail.ru")
+def is_registry(email):
+    connection = sqlite3.connect("general_database.db")
+    cursor = connection.cursor()
+    data = cursor.execute(
+        "SELECT * FROM Auth WHERE Email = (?)", (email,)).fetchone()
+    if data is None:
+        return False
+    else:
+        return True
+
+
+def data_correctly(nick, password):
+    connection = sqlite3.connect("general_database.db")
+    cursor = connection.cursor()
+    data = cursor.execute(
+        "SELECT * FROM Auth WHERE Email = (?) and Password = (?);", (nick, password))
+    if is_registry(nick) == 0:
+        return -1
+    elif data == "None":
+        return 1
+    else:
+        return 0
+
+
+def get_id_by_email(email: str):
+    connection = sqlite3.connect("general_database.db")
+    cursor = connection.cursor()
+    data = cursor.execute(
+        "SELECT UserId FROM General_info WHERE Email = (?);", (email,)).fetchone()
+    if data is None:
+        return 1
+    else:
+        return data[0]
+
+
+
